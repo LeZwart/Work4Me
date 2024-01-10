c8afa9e02978f3a53d82763b31b411777eccef0a
LeZwart <192443@novacollege.nl>
2024-01-10 11:03:05 +0100

    code update

--- www/index.php
+++ www/index.php
@@ -3,9 +3,29 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <link rel="stylesheet" href="main-style.css">
+        <link rel="stylesheet" href="index.css">
+        <title>Work4Me</title>
     </head>
     <body>
-        
+        <header class="homepage-header">
+            <h1>Work4Me</h1>
+            <nav class="header-nav">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="login.php">Log In</a></li>
+                    <li><a href="register.php">Register</a></li>
+                </ul>
+            </nav>
+        </header>
+
+        <main class="homepage-main">
+            <h2></h2>
+        </main>
+
+        <footer class="homepage-footer">
+            <p>Work4Me</p>
+            <p>Project door Leon Zwart</p>
+        </footer>
     </body>
 </html>
