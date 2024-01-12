d92afd620b176432a3c9077f35d3d1f898f7f35f
LeZwart <192443@novacollege.nl>
2024-01-12 13:35:09 +0100

    Display workouts

--- www/workouts.php
+++ www/workouts.php
@@ -29,10 +29,44 @@ $permission = new Permission();
                 </form>
             </section>
             <section class="workouts">
+                <!-- Dit is hoe een workout eruit ziet
                 <article class="workout-article">
                     <h2>Workout Naam</h2>
                     <img class="workout-img" src="images/500placeholder.png" alt="foto">
+                    <a class="workout-bekijk" href="#">Bekijk workout</a>
                 </article>
+                 -->
+
+                <?php
+
+                // TODO: Zoek workouts op basis van de zoekterm
+                $sql = "SELECT * FROM Workouts";
+                $result = mysqli_query($conn, $sql);
+
+                // Zet elke workout in de pagina
+                if (mysqli_num_rows($result) > 0) {
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $title = $row['Titel'];
+                        $image = $row['Afbeelding'];
+                        $id = $row['WorkoutID'];
+
+                        // Als er geen foto is dan wordt er een placeholder gebruikt
+                        $placeholder = "images/500placeholder.png";
+                        if ($image === "") {
+                            $image = $placeholder;
+                        }
+
+                        // Zet de workout in de pagina
+                        echo "<article class='workout-article'>";
+                        echo "<h2>" . $title . "</h2>";
+                        echo "<img class='workout-img' src='" . $image . "' alt='foto'>";
+                        echo "<a class='workout-bekijk' href='workout.php?id=" . $id . "'>Bekijk workout</a>";
+                        echo "</article>";
+                    }
+                } else {
+                    echo "<p>Geen workouts gevonden</p>";
+                }
+                ?>
             </section>
         </main>
 
