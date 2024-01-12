4929fab4ed365d62d3cae27aaf24870cf07096b8
LeZwart <192443@novacollege.nl>
2024-01-12 09:41:38 +0100

    oops forgot about the registration :3 teehee

--- www/register.php
+++ www/register.php
@@ -5,18 +5,20 @@
     require "permission.php";
     
     $permission = new Permission();
-    
+
     if(isset($_SESSION['user_id'])){
         header("Location: index.php");
     }
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="main-style.css">
-        <link rel="stylesheet" href="login.css">
+        <link rel="stylesheet" href="css/main-style.css">
+        <link rel="stylesheet" href="css/login.css">
         <title>Work4Me - Registratie</title>
     </head>
     <body>
