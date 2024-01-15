cc2493579aa6ee5393a641447487de7d588bb581
LeZwart <192443@novacollege.nl>
2024-01-15 10:58:21 +0100

    register front done

--- www/process-register.php
+++ www/process-register.php
@@ -2,4 +2,67 @@
 
 require "database.php";
 
-var_dump($_POST);
+// var_dump($_POST);
+
+// foreach ($_POST as $key => $value) {
+//     echo "<br>" . $key . " " . $value . "<br>";
+// }
+
+$voornaam = $_POST['Voornaam'];
+$tussenvoegsel = $_POST['Tussenvoegsel'];
+$achternaam = $_POST['Achternaam'];
+
+$geslacht = $_POST['geslacht'];
+
+$gebruikersnaam = $_POST['Gebruikersnaam'];
+$email = $_POST['email'];
+$wachtwoord = $_POST['password'];
+
+$straat = $_POST['straat'];
+$huisnummer = $_POST['huisnummer'];
+$postcode = $_POST['postcode'];
+$land = $_POST['land'];
+
+$telefoonnummer = $_POST['telefoonnummer'];
+$mobielnummer = $_POST['mobielnummer'];
+
+// Check if any of the variables are null
+if (
+ empty($voornaam) ||
+ empty($achternaam) ||
+ empty($geslacht) ||
+ empty($gebruikersnaam) ||
+ empty($email) ||
+ empty($wachtwoord) ||
+ empty($straat) ||
+ empty($huisnummer) ||
+ empty($postcode) ||
+ empty($land) ||
+ empty($mobielnummer)
+ ) 
+ {
+    echo "Vul alle velden in";
+    exit;
+ }
+
+// Check if email is not already used
+$sql = "SELECT * FROM `users` WHERE `email` = " . $email;
+$result = mysqli_query($conn, $sql);
+$emailcount = mysqli_num_rows($result);
+
+if ($emailcount > 0) {
+    echo "Email is al in gebruik";
+    exit;
+}
+
+// Check if username is not already used
+$sql = "SELECT * FROM `users` WHERE `username` =" . $gebruikersnaam;
+$result = mysqli_query($conn, $sql);
+$usernamecount = mysqli_num_rows($result);
+
+if ($usernamecount > 0) {
+    echo "Gebruikersnaam is al in gebruik";
+    exit;
+}
+
+// Insert user into database
--- www/register.php
+++ www/register.php
@@ -18,7 +18,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="css/main-style.css">
-        <link rel="stylesheet" href="css/login.css">
+        <link rel="stylesheet" href="css/register.css">
         <title>Work4Me - Registratie</title>
     </head>
     <body>
@@ -26,34 +26,62 @@
         <main>
             <form action="process-register.php" method="POST">
 
-                <!-- Naam -->
-                <label for="firstname">Naam</label>
-                <input type="text" name="firstname" id="firstname" placeholder="Voornaam">
-                <input type="text" name="infix" id="infix" placeholder="Tussenvoegsel">
-                <input type="text" name="lastname" id="lastname" placeholder="Achternaam">
+                <div class="information">
+                    <section>
+                            <!-- Naam -->
+                        <label for="Voornaam">Naam</label>
+                        <input type="text" name="Voornaam" id="Voornaam" placeholder="Voornaam">
+                        <input type="text" name="Tussenvoegsel" id="Tussenvoegsel" placeholder="Tussenvoegsel">
+                        <input type="text" name="Achternaam" id="Achternaam" placeholder="Achternaam">
 
-                <!-- Geslacht -->
-                <label for="gender">Geslacht</label>
-                <select name="gender" id="gender">
-                    <option value="">Wil ik niet zeggen</option>
-                    <option value="Man">Man</option>
-                    <option value="Vrouw">Vrouw</option>
-                </select>
+                        <!-- Geslacht -->
+                        <label for="geslacht">Geslacht</label>
+                        <select name="geslacht" id="geslacht">
+                            <option value="">Wil ik niet zeggen</option>
+                            <option value="Man">Man</option>
+                            <option value="Vrouw">Vrouw</option>
+                        </select>
 
-                <!-- Gebruikersnaam -->
-                <label for="username">Gebruikersnaam</label>
-                <input type="text" name="username" id="username" placeholder="Gebruikersnaam">
+                        <!-- Gebruikersnaam -->
+                        <label for="Gebruikersnaam">Gebruikersnaam</label>
+                        <input type="text" name="Gebruikersnaam" id="Gebruikersnaam" placeholder="Gebruikersnaam">
 
-                <!-- E-mail -->
-                <label for="email">E-mail</label>
-                <input type="email" name="email" id="email" placeholder="E-mail">
+                        <!-- E-mail -->
+                        <label for="email">E-mail</label>
+                        <input type="email" name="email" id="email" placeholder="E-mail">
 
-                <!-- Wachtwoord -->
-                <label for="password">Wachtwoord</label>
-                <input type="password" name="password" id="password" placeholder="Wachtwoord">
+                        <!-- Wachtwoord -->
+                        <label for="Wachtwoord">Wachtwoord</label>
+                        <input type="password" name="password" id="password" placeholder="Wachtwoord">
+                    </section>
+                    <!-- <br><br> -->
+                    <section>
+                        <label for="straat">Straat</label>
+                        <input type="text" name="straat" id="straat" placeholder="Straat">
+
+                        <label for="huisnummer">Huisnummer</label>
+                        <input type="text" name="huisnummer" id="huisnummer" placeholder="Huisnummer">
+
+                        <label for="postcode">Postcode</label>
+                        <input type="text" name="postcode" id="postcode" placeholder="Postcode">
+
+                        <label for="land">Land</label>
+                        <select name="land" id="land">
+                            <option value="Nederland">Nederland</option>
+                            <option value="België">België</option>
+                        </select>
+
+                        <label for="telefoonnummer">telefoonnummer</label>
+                        <input type="text" name="telefoonnummer" id="telefoonnummer" placeholder="Telefoonnummer">
+
+                        <label for="mobielnummer">mobielnummer</label>
+                        <input type="text" name="mobielnummer" id="mobielnummer" placeholder="Mobielnummer">
+
+                    </section>
+                </div>
 
 
-                <input type="submit" value="Log In">
+                <input type="submit" value="Maak account">
                 <a href="login.php" class="redirect-login-register">Heb je al een account? Log in!</a>
             </form>
         </main>
